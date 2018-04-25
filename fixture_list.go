63fbc22e074750d93d761e578e52ec7d3c834894
Andrew Knapp <ki4jnq@dknapp.com>
2018-04-25 15:26:55 -0400

    Clean FixtureList in reverse order.

--- fixture_list.go
+++ fixture_list.go
@@ -24,8 +24,12 @@ func (fl FixtureList) LoadTx(tx *sql.Tx) error {
 	return nil
 }
 
+// Clean deletes all records in effected tables in the reverse order that they
+// were loaded. This helps ensure that any child records are deleted before
+// the parent records they depend on are.
 func (fl FixtureList) Clean(db *sql.DB) error {
-	for _, f := range fl {
+	for i := len(fl) - 1; i >= 0; i-- {
+		f := fl[i]
 		if err := f.Clean(db); err != nil {
 			return err
 		}
